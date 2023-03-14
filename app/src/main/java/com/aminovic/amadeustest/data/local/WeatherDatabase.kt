81676cd20968f5cc1d31c99f575e0428c3d1d70b
aminovicnouri <Me@dubai2020>
2023-03-14 21:42:05 +0400

    setup dagger hilt

--- app/src/main/java/com/aminovic/amadeustest/data/local/WeatherDatabase.kt
+++ app/src/main/java/com/aminovic/amadeustest/data/local/WeatherDatabase.kt
@@ -1,12 +1,13 @@
 package com.aminovic.amadeustest.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.aminovic.amadeustest.data.local.entity.CityEntity
 
 @Database(
     entities = [CityEntity::class],
     version = 1
 )
-abstract class WeatherDatabase {
+abstract class WeatherDatabase: RoomDatabase() {
     abstract val dao: WeatherDao
 }
---
+++ app/src/main/java/com/aminovic/amadeustest/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.aminovic.amadeustest.di
+
+import android.app.Application
+import androidx.room.Room
+import com.aminovic.amadeustest.data.local.WeatherDatabase
+import com.aminovic.amadeustest.data.remote.WeatherApi
+import com.aminovic.amadeustest.data.repository.WeatherRepositoryImpl
+import com.aminovic.amadeustest.domain.repository.WeatherRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl("http://127.0.0.1:5500/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherDatabase(app: Application): WeatherDatabase {
+        return Room.databaseBuilder(
+            app,
+            WeatherDatabase::class.java,
+            "weather_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(api: WeatherApi, database: WeatherDatabase): WeatherRepository {
+        return WeatherRepositoryImpl(api = api, dao = database.dao)
+    }
+}
